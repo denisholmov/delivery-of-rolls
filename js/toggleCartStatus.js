9b78399e55b884790af93f2b03b9b1f542760f93
denis <denisskholmov@gmail.com>
2025-08-21 15:32:33 +0300

    убрал повторяющийся код

--- js/toggleCartStatus.js
+++ js/toggleCartStatus.js
@@ -1,10 +1,14 @@
 function toggleCartStatus() {
   const cartWrapperNode = document.querySelector('.cart-wrapper');
   const dataCartEmptyNode = document.querySelector('[data-cart-empty]');
+  const orderFormNode = document.querySelector('#order-form');
+  const cardBodyNode = cartWrapperNode.closest('.card-body');
 
   const isEmpty = cartWrapperNode.children.length <= 0;
 
   if (isEmpty) {
+    orderFormNode.remove();
+
     cartWrapperNode.insertAdjacentHTML(
       'beforebegin',
       `
@@ -17,5 +21,20 @@ function toggleCartStatus() {
 
   if (!isEmpty) {
     dataCartEmptyNode.remove();
+
+    cardBodyNode.insertAdjacentHTML(
+      'afterend',
+      `
+      <div id="order-form" class="card-body border-top">
+						<h5 class="card-title">Оформить заказ</h5>
+						<form>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Ваш номер телефона">
+							</div>
+							<button type="submit" class="btn btn-primary">Заказать</button>
+						</form>
+		</div>
+    `
+    );
   }
 }
